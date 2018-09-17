111e23077c4abfd8a29948527ba0ce06f9447a2f
DESKTOP-44D12JS\Asus <marian.zoicas@gmail.com>
2018-09-17 09:35:42 +0200

    working process

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var express = require("express");
-var app= express();
-var port = process.env.PORT || 3000;
+let express = require("express");
+let app= express();
+let port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-    app.get("/", function(req, res){
-        res.send("Welcome to Monsterr's page!");
+ ///////  routes
+//  "/"
+app.get("/", function (req, res) {
+    res.render("home")
+
 });
+// /documentation
+app.get("/documentation", function (req, res) {
+    res.render("documentation")
+});
+// games
+app.get("/games", function (req, res) {
+    res.render("games")
+})
+//aboutus
+app.get("/aboutus", function (req, res) {
+    res.render("aboutus")
+})
 
+  // connecting to port
     app.listen(port);
